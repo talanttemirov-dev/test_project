486eeb478a89ba762b25634137a908a19d702c74
ttemirov <talant3689@mail.ru>
2024-11-13 12:58:39 +0500

    Initial commit

---
+++ src/main/java/com/example/test_github/TestGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_github/TestGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
